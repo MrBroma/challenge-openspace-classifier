d7bbdfe4d105e44d4aaf3b6a73e92b076b089280
Loic Rouaud <loic.rouaud@gmail.com>
2024-06-13 11:55:19 +0200

    table.py with class Table

---
+++ utils/table.py
@@ -0,0 +1,18 @@
+# class Table --> manage the capacity and seats
+class Table:
+    def __init__(self, capacity, seats) -> None:
+        self.capacity = capacity
+        self.seats = seats
+        
+    
+    def has_free_spot(capacity):
+        return True
+        pass
+
+    def assign_seat(name):
+        pass
+
+    def left_capacity():
+        return number_seats
+        pass
+
